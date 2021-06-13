a006ef2bf1a4d4b5bf876cb4d668e41d4a9ffda7
dagidagi1 <arkadya@ac.sce.ac.il>
2021-06-13 13:12:50 +0300

    a

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 #author 1 Arkadi yakubov 2088064162
 #author 2 Mikhail Diachkov 336426176
+def suffix(num):
+    suf = 0.00000131250
+
+
+def num_after_point(x):
+    s = str(x)
+    if not '.' in s:
+        return 0
+    return len(s) - s.index('.') - 1
+
+def eps():
+    eps = 1
+    while 1 + eps > 1:
+        eps /= 2
+    eps *= 2
+    return eps
+
+print(num_after_point(eps()))
